1420567b272d3eaf6c57c30540a2404106c530af
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-01-27 01:14:19 +0200

    feat: add favicon and keep custom fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/logo.png" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${tajawal.variable} antialiased`}
       >
