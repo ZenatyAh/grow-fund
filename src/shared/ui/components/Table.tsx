11125f37b4e8618a3df178304aa607294e5ce95c
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2026-02-17 19:39:11 +0200

    feat(Table): Add otherClassName prop

--- src/shared/ui/components/Table.tsx
+++ src/shared/ui/components/Table.tsx
@@ -11,9 +11,10 @@ const Table = ({
   showEdit,
   showActionsColumn = true,
   deleteLocation,
+  otherClassName,
 }: TableProps) => {
   return (
-    <div className="bg-white rounded-2xl border border-(--bg-slate-100) p-4 w-full">
+    <div className={`bg-white rounded-2xl border border-(--bg-slate-100) p-4 w-full ${otherClassName}`}>
       <div className="flex items-center justify-between gap-3 mb-2 px-6 py-4">
         <h1 className="text-black font-bold text-lg">{title}</h1>
         <Link href="#" className="text-(--bg-bold-blue)">
